73e223bb2016df624c0955a73dcbdfa0dc8f39e5
Paarmita Bhargava <16ucc065@lnmiit.ac.in>
2018-01-16 23:33:04 +0530

    Update 20180116104543_create_users.rb

--- db/migrate/20180116104543_create_users.rb
+++ db/migrate/20180116104543_create_users.rb
@@ -1,11 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string, :email
-      t.string, :password_digest
+      t.string :email, unique: true, null: false
+      t.string :password_digest, null: false
       t.timestamp :confirmed_at
- 
+
       t.timestamps null: false
     end
   end
-end
